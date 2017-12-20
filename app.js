977f2c3aa8a440a3c3111df53551015704957299
Vitor Figueredo <vitor.figueredo18@gmail.com>
2017-12-20 15:33:28 -0200

    New javascript added

--- app.js
+++ app.js
@@ -1,59 +1,24 @@
-(function($){
-  var date    = new Date(),
-      month   = date.getMonth();
-      day     = date.getDate(),
-      weekDay = date.getDay(),
-      hours   = {
-        start: new Date(date.getFullYear(), month, day),
-        end: new Date(date.getFullYear(), month, day)
-      };
-  
-  // weekDay var [0 = sun, 1 = mon, 2 = tues ... 5 = fri 6 = sat]
-  
-  // If it's Monday - Friday
-  if(weekDay >= 1 && weekDay <= 5){
+(function() {
+  var start = new Date;
+  start.setHours(23, 59, 59); // 11pm
 
-    // Start at 7am, end at 8pm
-    hours.start.setHours(1);
-    hours.end.setHours(23);
-
-  // If it's Saturday
-  } else if(weekDay == 6){
-
-    // Start at 8am, end at 8pm
-    hours.start.setHours(1);
-    hours.end.setHours(23);
-
-  // If it's Sunday
-  } else {
-
-    // Start at 9am, end at 6pm
-    hours.start.setHours(1);
-    hours.end.setHours(23);
+  function pad(num) {
+    return ("0" + parseInt(num)).substr(-2);
   }
 
-  function countDown(){
-    var date         = new Date(),
-        countHours   = ('0' + (hours.end.getHours() - date.getHours())).substr(-2),
-        countMinutes = ('0' + (59 - date.getMinutes())).substr(-2),
-        countSeconds = ('0' + (59 - date.getSeconds())).substr(-2);
-
-    // If it's currently not within the hours, don't show the countdown
-    if(date.getHours() < hours.start.getHours() || date.getHours() > hours.end.getHours()){
-      $('.countdown').hide();
-    } else if($('.countdown').not(':visible')){
-      $('.countdown').show();
+  function tick() {
+    var now = new Date;
+    if (now > start) { // too late, go to tomorrow
+      start.setDate(start.getDate() + 1);
     }
-
-    $('.countdown .hours').text(countHours);
-    $('.countdown .minutes').text(countMinutes);
-    $('.countdown .seconds').html(countSeconds);
-
+    var remain = ((start - now) / 1000);
+    var hh = pad((remain / 60 / 60) % 60);
+    var mm = pad((remain / 60) % 60);
+    var ss = pad(remain % 60);
+    document.getElementById('time').innerHTML =
+      hh + " hours: " + mm + " minutes: " + ss + " seconds";
+    setTimeout(tick, 1000);
   }
 
-  $(function(){
-    setInterval(function(){
-      countDown();
-    }, 1000);
-  });
-})(jQuery);
+  document.addEventListener('DOMContentLoaded', tick);
+})();
